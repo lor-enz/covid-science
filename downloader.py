d1401c203f048b7d313ba82586bb8d4e3a0365f3
lor-enz <git@lorenz.kiwi>
2021-03-29 02:39:03 +0200

    Add ags.json to downloader

--- downloader.py
+++ downloader.py
@@ -17,6 +17,29 @@ CSV_INF = {
     'file': 'data/inf.csv'
 }
 
+JSON_AGS = {
+    'url': 'https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/ags.json',
+    'file': 'data/ags.json'
+}
+
+# link wrong!
+CSV_MUC_INZ = {
+    'url': 'https://e.infogram.com/d2725960-06c5-4f17-8626-871d208db126?parent_url=https%3A%2F%2Fwww.muenchen.de%2Frathaus%2FStadtinfos%2FCoronavirus-Fallzahlen.html&src=embed#',
+    'file': 'data/muc-inz.csv'
+    # formatted very badly! >:(
+}
+
+def fix_comma_in_muc_infogram_csv(filename):
+    fin = open(filename, "rt")
+    data = fin.read()
+
+    data = data.replace('","', ';')
+    data = data.replace('\"', '')
+    fin.close()
+    fin = open(filename, "wt")
+    fin.write(data)
+    fin.close()
+
 
 def fix_comma_in_csv(filename):
     fin = open(filename, "rt")
@@ -31,17 +54,30 @@ def fix_comma_in_csv(filename):
     fin.close()
 
 
-def download_data(csv_data):
+def download_csv(csv_data):
     response = requests.get(csv_data['url'])
 
     with open(csv_data['file'], 'w') as f:
         writer = csv.writer(f)
         for line in response.iter_lines():
             writer.writerow(line.decode('utf-8').split(','))
-        print(f'Downloaded {csv_data["file"]}')
+        print(f'Downloaded csv: {csv_data["file"]}')
 
 
-download_data(CSV_VAC)
-download_data(CSV_INF_OLD)
-download_data(CSV_INF)
-fix_comma_in_csv('arcgisinf.csv')
+def download_file(file_data):
+    response = requests.get(file_data['url'])
+    with open(file_data['file'], 'wb') as f:
+        f.write(response.content)
+
+    print(f'Downloaded {file_data["file"]}')
+
+
+download_file(CSV_VAC)
+download_file(CSV_INF)
+download_file(JSON_AGS)
+
+#download_file(CSV_MUC_INZ)
+#fix_comma_in_muc_infogram_csv(CSV_MUC_INZ['file'])
+
+download_file(CSV_INF_OLD)
+fix_comma_in_csv(CSV_INF_OLD['file'])
